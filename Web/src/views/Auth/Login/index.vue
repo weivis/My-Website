da65d7b2604d697e0b4d35365cfa92675c06b89c
ShenVi <yvban@outlook.com>
2020-03-31 12:07:40 +0800

    20203311207

--- Web/src/views/Auth/Login/index.vue
+++ Web/src/views/Auth/Login/index.vue
@@ -1,7 +1,28 @@
 <template>
 
-  <div class="main">
-    login
+  <div class="Main">
+      <vue-particles
+      color="#409EFF"
+      :particleOpacity="0.7"
+      :particlesNumber="60"
+      shapeType="circle"
+      :particleSize="4"
+      linesColor="#409EFF"
+      :linesWidth="1"
+      :lineLinked="true"
+      :lineOpacity="0.4"
+      :linesDistance="150"
+      :moveSpeed="2"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+      class="lizi"
+    >
+    </vue-particles>
+
+  <div class="main-content width-normal">
+  </div>
   </div>
 
 </template>
@@ -15,6 +36,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+  .app-main{
+    margin-top: 0px !important;
+  }
+  .layout-header{
+    position: initial !important;
+    border-bottom: 0 !important;
+    box-shadow: initial !important;
+  }
+</style>
 
+<style scoped>
+  .Main{}
 </style>
